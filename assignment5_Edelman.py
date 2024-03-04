08faeb85959f10223b7ce5708e550787b10eeb74
bedelman073 <160267508+bedelman073@users.noreply.github.com>
2024-03-04 12:52:45 -0500

    Update assignment5_Edelman.py
    Creating the first two functions

--- assignment5_Edelman.py
+++ assignment5_Edelman.py
@@ -1,1 +1,9 @@
-
+# creating a class for basic math operations
+class BasicMathOperations:
+    # creating a function that takes in a first name and last name as inputs and returns a greeting
+    def GreetUser(self,f_n,l_n):
+        print(f"Hello {f_n} {l_n}")
+    # creating a function that takes in two numbers and returns the sum
+    def AddNumbers(self,num1,num2):
+        return num1+num2
+    # creating a function that adds, subtracts, multiplies, or divides a number based on a string input
