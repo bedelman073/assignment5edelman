cdd6abc6cfe116ee57f51bf1c0f916a37223b719
bedelman073 <160267508+bedelman073@users.noreply.github.com>
2024-03-04 12:53:12 -0500

    Update assignment5_Edelman.py
    Creating additional functions

--- assignment5_Edelman.py
+++ assignment5_Edelman.py
@@ -7,3 +7,26 @@ class BasicMathOperations:
     def AddNumbers(self,num1,num2):
         return num1+num2
     # creating a function that adds, subtracts, multiplies, or divides a number based on a string input
+    def PerformOp(self,num1,num2,operator):
+        if operator.upper()=="A":
+            return num1+num2
+        if operator.upper()=="S":
+            return num1-num2
+        if operator.upper()=="M":
+            return num1*num2
+        if operator.upper()=="D":
+            if num2==0:
+                return "infinity"
+            else:
+                return num1/num2
+    # creating a function that returns the square of a number
+    def calculateSqare(self,num):
+        return num**2
+    # creating a function that retuns the factorial of a number
+    def factorial(self,num):
+        if num==0:
+            return 1
+        fact=1
+        for i in range(1,num+1):
+            fact*=i
+        return fact
